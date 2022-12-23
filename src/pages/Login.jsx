00b7f530587a3938e68df3a0dd78cb74b25b6aca
vanhau <khaihau1002@gmail.com>
2022-12-23 17:07:46 +0700

    design page register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const Title = styled.h1``;
+const Form = styled.from``;
+const Input = styled.input``;
+const Agreement = styled.span``;
+
+const Button = styled.button``;
+
+const Login = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title></Title>
+                <Form>
+                    <Input placeholder='first name' />
+                    <Input placeholder='last name' />
+                    <Input placeholder='username' />
+                    <Input placeholder='email' />
+                    <Input placeholder='password' />
+                    <Input placeholder='confirm password' />
+                    <Agreement>
+                        By creating an account, I consent to the processing of
+                        my personal data in accordance with the{' '}
+                        <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Login;
